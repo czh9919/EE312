bd9cafeed4a63b57fe2252fde1768e2e4dfcda6b
czh9919 <czh9919@outlook.com>
2020-03-21 15:45:00 +0800

    frame init

--- lab1/RTL/alu.v
+++ lab1/RTL/alu.v
@@ -9,5 +9,61 @@ module ALU(A,B,OP,C,Cout);
 	output Cout;
 
 	//TODO
+	always @(*) begin
+		case (OP)
+			// !Arithmetic
+			OP_ADD: begin
+				//TODO
+			end
+			OP_SUB: begin
+				//TODO
+			end
+			// !Bitwise Boolean operation
+			OP_AND: begin
+				//TODO
+			end
+			OP_OR: begin
+				//TODO
+			end
+			OP_NAND: begin
+				//TODO
+			end
+			OP_NOR: begin
+				//TODO
+			end
+			OP_XOR: begin
+				//TODO
+			end
+			OP_XNOR: begin
+				//TODO
+			end
+			// !Logic
+			OP_ID: begin
+				//TODO
+			end
+			OP_NOT: begin
+				//TODO
+			end
+			// !shift
+			OP_LRS: begin
+				//TODO
+			end
+			OP_ARS: begin
+				//TODO
+			end
+			OP_RR: begin
+				//TODO
+			end
+			OP_LLS: begin
+				//TODO
+			end
+			OP_ALS: begin
+				//TODO
+			end
+			OP_RL: begin
+				//TODO
+			end
+		endcase
+	end
 	
 endmodule
