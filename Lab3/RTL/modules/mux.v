fba733a759776edd789b050636423d6b7ffad54d
Cui Yx <CYX981129@163.com>
2020-04-17 21:32:57 +0800

    mux

---
+++ Lab3/RTL/modules/mux.v
@@ -0,0 +1,16 @@
+module MUX 
+(
+	input	wire	[9:0]       CON,//chip select negative??
+	input	wire	[5:0]		DI1, //data in
+    input   wire    [5:0]       DI,
+	output	wire	[5:0]		DOUT // data out
+);
+	always @ (*) begin
+		// Asynchronous read
+		if (CON)
+			DOUT=DI;
+        else
+            DOUT=DI1;
+	end
+
+endmodule
