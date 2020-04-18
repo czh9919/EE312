a4dfeb12f263df3a4be0dd8b9c3da5c7237adc96
Cui Yx <CYX981129@163.com>
2020-04-18 15:14:41 +0800

    CHANGE

--- lab3/RTL/modules/ADD.v
+++ lab3/RTL/modules/ADD.v
@@ -1,13 +1,10 @@
 module ADD 
-(
-	input	wire	[9:0]       CON,//chip select negative??
-	input	wire	[5:0]		DI1, //data in
+(  
+	input	wire	[5:0]		DI1, 
     input   wire    [5:0]       DI,
-	output	wire	[5:0]		DOUT // data out
+	output	wire	[5:0]		DOUT 
 );
 	always @ (*) begin
-		// Asynchronous read
-		if (CON)
 			DOUT=DI1+DI;
 	end
 
