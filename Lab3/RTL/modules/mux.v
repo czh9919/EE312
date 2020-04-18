d79d19b923a05c0f7ea812387ac71fdc2971bec9
czh9919 <czh9919@outlook.com>
2020-04-18 13:47:26 +0800

    try init

--- Lab3/RTL/modules/mux.v
+++ Lab3/RTL/modules/mux.v
@@ -1,16 +1,18 @@
-module MUX 
+module MUX #(
+	parameter DWITH
+)
 (
-	input	wire	[9:0]       CON,//chip select negative??
-	input	wire	[5:0]		DI1, //data in
-    input   wire    [5:0]       DI,
-	output	wire	[5:0]		DOUT // data out
+	input	wire	CON,//chip select negative??
+	input	wire	[DWITH-1:0]		DI1, //data in
+	input	wire	[DWITH-1:0]		DI,
+	output	wire	[DWITH-1:0]		DOUT // data out
 );
 	always @ (*) begin
 		// Asynchronous read
 		if (CON)
 			DOUT=DI;
-        else
-            DOUT=DI1;
+		else
+			DOUT=DI1;
 	end
 
 endmodule
