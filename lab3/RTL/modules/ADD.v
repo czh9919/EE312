373ac55511b59b26da727eb888f8a42884ad8dcd
czh9919 <czh9919@outlook.com>
2020-04-24 22:41:34 +0800

    修改

--- lab3/RTL/modules/ADD.v
+++ lab3/RTL/modules/ADD.v
@@ -2,9 +2,9 @@ module ADD #(
 	parameter DWIDTH
 )
 (
-	input	wire	[DWIDTH:0]		DI, //data in
-	input	wire	[DWIDTH:0]		DI1,
-	output	reg	[DWIDTH:0]		DOUT // data out
+	input	wire	[DWIDTH-1:0]		DI, //data in
+	input	wire	[DWIDTH-1:0]		DI1,
+	output	reg	[DWIDTH-1:0]		DOUT // data out
 );
 	always @ (*) begin
 		DOUT[DWIDTH:0]=DI1[DWIDTH:0]+DI[DWIDTH:0];
