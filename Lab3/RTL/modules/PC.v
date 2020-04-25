08cad6f798a1dc353bc295d97cf0cb3f57254a1d
czh9919 <czh9919@outlook.com>
2020-04-26 00:48:30 +0800

    recover

--- Lab3/RTL/modules/PC.v
+++ Lab3/RTL/modules/PC.v
@@ -9,15 +9,15 @@ module PC (
 
 always @(posedge clk or negedge rstn) begin
     O_MEM_ADD=12'b0;
-    I_MEM_CSN = 0;
-    D_MEM_CSN =0;
+    I_MEM_CSN = 1;
+    D_MEM_CSN =1;
 end
 
 always @(posedge clk) begin
     if (~rstn) begin
         O_MEM_ADD[11:0]=I_MEM_ADD[11:0];
-        I_MEM_CSN = 1;
-        D_MEM_CSN =1;
+        I_MEM_CSN = 0;
+        D_MEM_CSN =0;
     end
 end
 
