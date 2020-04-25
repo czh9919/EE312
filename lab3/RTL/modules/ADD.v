82d11d3ce32b5d8f79f9df1e2ae7c1bbaa9d2292
czh9919 <czh9919@outlook.com>
2020-04-25 21:15:47 +0800

    remove

--- lab3/RTL/modules/ADD.v
+++ lab3/RTL/modules/ADD.v
@@ -6,7 +6,7 @@ module ADD #(
 	input  wire rstn,
 	input	wire	[DWIDTH-1:0]		DI, //data in
 	input	wire	[DWIDTH-1:0]		DI1,
-	output	reg	[DWIDTH-1:0]		DOUT // data out
+	output	reg		[DWIDTH-1:0]		DOUT // data out
 );
 
 always @(posedge clk) begin
@@ -15,7 +15,10 @@ always @(posedge clk) begin
 	end
 end
 always @ (*) begin
-	DOUT[DWIDTH:0]=DI1[DWIDTH:0]+DI[DWIDTH:0];
+	DOUT[DWIDTH-1:0]=DI1[DWIDTH-1:0]+DI[DWIDTH-1:0];
+	$display("%b has been passed",DI1);
+	$display("%b has been passed",DI);
+	$display("%b has been passed",DOUT);
 end
 
 endmodule
