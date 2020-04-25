fa9882ea27f1ff7b022ad2109c6182fac2936753
czh9919 <czh9919@outlook.com>
2020-04-25 21:52:38 +0800

    repair

--- Lab3/RTL/modules/ALU.v
+++ Lab3/RTL/modules/ALU.v
@@ -10,70 +10,70 @@ module ALU(
 	always @(*) begin
 		case (OP)
 			// !Arithmetic
-			5'b00000: begin
+			4'b0000: begin
 				C=A+B;
 				Cout = ( A == B ) ;
 				//Add/BEQ
 			end
-			5'b00001: begin
+			4'b0001: begin
 				C = (A>=B)?(A-B):(~(B-A-32'h1));
 				//sub
 				Cout = 0;
 			end
 			// !Bitwise Boolean operation
-			5'b00010: begin
+			4'b0010: begin
 				C=A & B;
 				Cout=0;
 				//and
 			end
-			4'b00011: begin
+			4'b0011: begin
 				C = A|B;
 				//or
 				Cout=0;
 			end
-			4'b00100: begin
+			4'b0100: begin
 				if (A<B)
 					Cout=1;
 				else
 					Cout=0;
 				//sltu
 			end
-			4'b00101: begin
+			4'b0101: begin
 				C = A << B;
 				Cout=0;
 				//SLL
 			end
-			4'b00110: begin
+			4'b0110: begin
 				C=A^B;
 				//xor
 				Cout=0;
 			end
-			4'b00111: begin
+			4'b0111: begin
 				C = A >>> B;
 				Cout=0;
 				//SRA
 			end
 			// !Logic
-			4'b01000: begin
+			4'b1000: begin
 				C=A+(B<<12);
 				Cout=0;
 				//ALUPC
 			end
-			4'b01001: begin
+			4'b1001: begin
 				C=0;
 				Cout = 0;
 				//JAL
 			end
 			// !shift
-			4'b01010: begin
+			4'b1010: begin
 				C=A >> B ;
 				//srl
 			end
-			4'b01011: begin 
-				C = A + 4; 
+			4'b1011: begin 
+				Cout = A + 4; 
 				//JALR
 			end
-			4'b01100: begin
+			4'b1100: begin
 				if (A[0]==B[0])
 					if (A[4:1]<B[4:1])
 						Cout=1;
@@ -83,16 +83,16 @@ module ALU(
 					Cout=0;
 				//SLT
 			end
-			4'b01101: begin
+			4'b1101: begin
 				C = A <<12;
 				Cout = ( A < B ); 
 				//LUI
 			end
-			4'b01110: begin
+			4'b1110: begin
 				Cout= ( A != B ) ;
 				//BNE
 			end
-			4'b01111: begin
+			4'b1111: begin
 				
 				Cout= ( A >=B) ;
 				//BGE/BLT
