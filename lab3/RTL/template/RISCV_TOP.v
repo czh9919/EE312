2226b0ea6c1d3521c3ca9b3edf5c43da6bac1f95
czh9919 <czh9919@outlook.com>
2020-04-25 19:41:24 +0800

    PC_4 init

--- lab3/RTL/template/RISCV_TOP.v
+++ lab3/RTL/template/RISCV_TOP.v
@@ -85,7 +85,8 @@ module RISCV_TOP (
 		.I_MEM_ADD(Back_to_PC),
 		.O_MEM_ADD(OUT_PC),
 		.I_MEM_CSN(I_MEM_CSN),
-		.D_MEM_CSN(D_MEM_CSN)
+		.D_MEM_CSN(D_MEM_CSN),
+		.PC_4_to_MUX(PC_4_to_MUX)
 	);
 	always @(*) begin
 		I_MEM_ADDR=OUT_PC;
