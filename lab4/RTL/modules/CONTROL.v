36cc686b01c15fa68635213b7e080d73063ff7bf
Cui Yx <CYX981129@163.com>
2020-05-09 13:38:42 +0800

    change state

--- lab4/RTL/modules/CONTROL.v
+++ lab4/RTL/modules/CONTROL.v
@@ -27,37 +27,86 @@ always @(rstn) begin
     I_MEM_write=0;
     sign_ex=0;
 end
-
-always @(*) begin
-    if (temp_I[31:25]==7'b0000000&&temp_I[6:0]==7'b0110011)begin
-        if(state==2'b 01)begin
-            PC_source=1;//PC+4
-            MUX_A=0;
-            MUX_B=2'b10;
-            RegWrite=0;
-            MemWrite=0;
-            Reg_MUX=1;
-            ALUOp=4'b0000;
-            I_MEM_write=1;
-            sign_ex=0;
-        end
-        if(state==2'b 10)begin
+always @(*) begin 
+    if(state==2'b 01)begin//我感觉01的时候所有的指令都是一样的。。。
+        PC_source=1;//PC+4
+        MUX_A=0;
+        MUX_B=2'b10;
+        RegWrite=0;
+        MemWrite=0;
+        Reg_MUX=1;
+        ALUOp=4'b0000;
+        I_MEM_write=1;
+        sign_ex=0;
+    end
+    if (state==2'b10)begin
+        if (temp_I[31:25]==7'b0000000&&temp_I[6:0]==7'b0110011)begin//AND 
             PC_source=0;
             MUX_A=1;
-            MUX_B=2'b00;
-            RegWrite=0;
+            MUX_B=2'b01;
+            RegWrite=1;
             MemWrite=0;
             Reg_MUX=1;
             I_MEM_write=0;
-            sign_ex=0; 
+            sign_ex=0;
         end
-        if(state==2'b 11)begin
+        if (temp_I[31:25]==7'b0100000&&temp_I[6:0]==7'b0110011)begin//sub
             PC_source=0;
             MUX_A=1;
-            MUX_B=2'b00;
+            MUX_B=2'b01;
+            RegWrite=1;
+            MemWrite=0;
+            Reg_MUX=1;
+            I_MEM_write=0;
+            sign_ex=0;
+        end
+        if (temp_I[6:0]==7'b0010011)begin//ANDI
+            PC_source=0;
+            MUX_A=1;
+            MUX_B=2'b01;
+            RegWrite=1;
+            MemWrite=0;
+            Reg_MUX=1;
+            I_MEM_write=0;
+            sign_ex=1;
+        end
+        if (temp_I[31:25]==7'b0100000&&temp_I[6:0]==7'b0010011)begin//SRI...
+            PC_source=0;
+            MUX_A=1;
+            MUX_B=2'b01;
+            RegWrite=1;
+            MemWrite=0;
+            Reg_MUX=1;
+            I_MEM_write=0;
+            sign_ex=0;
+        end
+        if (temp_I[31:25]==7'b0000000&&temp_I[6:0]==7'b0010011)begin//SRLI  SLLI
+            PC_source=0;
+            MUX_A=1;
+            MUX_B=2'b01;
+            RegWrite=1;
+            MemWrite=0;
+            Reg_MUX=1;
+            I_MEM_write=0;
+            sign_ex=0;
+        end
+        //BEQ.........
+        //_________________________________//
+
+
+
+
+
+    end
+
+    if(state==2'b 11)begin
+        if (temp_I[31:25]==7'b0000000&&temp_I[6:0]==7'b0110011)begin
+            PC_source=0;
+            MUX_A=1;
+            MUX_B=2'b01;
             RegWrite=0;
             MemWrite=0;
-            Reg_MUX=0;
+            Reg_MUX=1;
             if(temp_I[14:12]==3'b111)begin//ADD
                 ALUOp=4'b0000;
             end
@@ -68,7 +117,7 @@ always @(*) begin
                 ALUOp=4'b0011;
             end
             if(temp_I[14:12]==3'b100)begin//XOR
-                ALUOp=4'b1101;//!xor在哪
+                ALUOp=4'b1101;
             end
             if(temp_I[14:12]==3'b010)begin//SLT
                 ALUOp=4'b0111;
@@ -83,93 +132,28 @@ always @(*) begin
                 ALUOp=4'b0100;
             end
             I_MEM_write=0;
-            sign_ex=0; 
-        end
-        if(state==2'b 00)begin
-            PC_source=0;//pc+4
-            MUX_A=1;
-            MUX_B=2'b00;
-            RegWrite=1;
-            MemWrite=1;
-            Reg_MUX=1;
-            I_MEM_write=1;
             sign_ex=0;
         end
-    end
-    if (temp_I[31:25]==7'b0100000&&temp_I[6:0]==7'b0110011)begin//sub
-        if(state==2'b 01)begin
-            PC_source=1;//PC+4
-            MUX_A=0;
-            MUX_B=2'b10;
+        //_________________________________________________________________//
+        if (temp_I[31:25]==7'b0100000&&temp_I[6:0]==7'b0110011)begin//sub
+            PC_source=0;
+            MUX_A=1;
+            MUX_B=2'b01;
             RegWrite=0;
             MemWrite=0;
             Reg_MUX=1;
-            ALUOp=4'b0000;
-            I_MEM_write=1;
-            sign_ex=0;
-        end
-        if(state==2'b 10)begin
-            PC_source=0;
-            MUX_A=1;
-            MUX_B=2'b00;
-            RegWrite=1;
-            MemWrite=0;
-            Reg_MUX=1;
-            I_MEM_write=0;
-            sign_ex=0; 
-        end
-        if(state==2'b 11)begin
-            PC_source=0;
-            MUX_A=1;
-            MUX_B=2'b00;
-            RegWrite=0;
-            MemWrite=0;
-            Reg_MUX=0;
             if(temp_I[14:12]==3'b000)begin//SUB
                 ALUOp=4'b0001;
             end
             if(temp_I[14:12]==3'b101)begin//SRA
                 ALUOp=4'b0110;
             end
-            I_MEM_write=0;
-            sign_ex=0; 
-        end
-        if(state==2'b 00)begin
-            PC_source=0;//pc+4
-            MUX_A=1;
-            MUX_B=2'b00;
-            RegWrite=1;
-            MemWrite=1;
-            Reg_MUX=1;
             I_MEM_write=1;
             sign_ex=0;
         end
-    end
-
-    if (temp_I[6:0]==7'b0010011)begin//ANDI
-        if(state==2'b 01)begin
-            PC_source=1;//PC+4
-            MUX_A=0;
-            MUX_B=2'b10;
-            RegWrite=0;
-            MemWrite=0;
-            Reg_MUX=1;
-            ALUOp=4'b0000;
-            I_MEM_write=1;
-            sign_ex=0;
-        end
-        if(state==2'b 10)begin
-            PC_source=0;//PC+4
-            MUX_A=1;
-            MUX_B=2'b10;
-            RegWrite=1;
-            MemWrite=0;
-            Reg_MUX=1;
-            I_MEM_write=0;
-            sign_ex=1;
-        end
-        if(state==2'b 11)begin
-            PC_source=0;//PC+4
+        //___________________________________________________________________//
+        if (temp_I[6:0]==7'b0010011)begin//ANDI
+            PC_source=0;
             MUX_A=1;
             MUX_B=2'b01;
             RegWrite=0;
@@ -193,225 +177,62 @@ always @(*) begin
             if(temp_I[14:12]==3'b000)begin//ADDI
                 ALUOp=4'b0000;
             end
-            I_MEM_write=0;
+            I_MEM_write=1;
             sign_ex=1;
         end
-        if(state==2'b 00)begin
-            PC_source=0;//PC+4
+        //_________________________________________________________//
+        if (temp_I[31:25]==7'b0100000&&temp_I[6:0]==7'b0010011)begin
+            PC_source=0;
             MUX_A=1;
             MUX_B=2'b01;
             RegWrite=0;
-            MemWrite=1;
+            MemWrite=0;
             Reg_MUX=1;
-            I_MEM_write=0;
-            sign_ex=1;
+            if(temp_I[14:12]==3'b101)begin//SRAI
+                ALUOp=4'b0110;
+            end
+            I_MEM_write=1;
+            sign_ex=0;
         end
-    end
-
-    if (temp_I[31:25]==7'b0100000&&temp_I[6:0]==7'b0010011)begin
-        if(state==2'b 01)begin
-            PC_source=1;//PC+4
+        //_________________________________________________________________//
+        if (temp_I[31:25]==7'b0000000&&temp_I[6:0]==7'b0010011)begin
+            PC_source=1;
             MUX_A=0;
             MUX_B=2'b10;
             RegWrite=0;
             MemWrite=0;
             Reg_MUX=1;
-            ALUOp=4'b0000;
-            I_MEM_write=1;
-            sign_ex=0;
-        end       
-        if(state==2'b 10)begin
-            PC_source=0;
-            MUX_A=1;
-            MUX_B=2'b00;
-            RegWrite=1;
-            MemWrite=0;
-            Reg_MUX=1;
-            I_MEM_write=0;
-            sign_ex=0; 
-        end
-        if(state==2'b 11)begin
-            PC_source=0;
-            MUX_A=1;
-            MUX_B=2'b00;
-            RegWrite=0;
-            MemWrite=0;
-            Reg_MUX=0;
-            if(temp_I[14:12]==3'b101)begin//SRAI
-                ALUOp=4'b0110;
-            end
             if(temp_I[14:12]==3'b101)begin//SRLI
                 ALUOp=4'b0101;
             end
             if(temp_I[14:12]==3'b001)begin//SLLI
                 ALUOp=4'b0100;
             end
-            I_MEM_write=0;
-            sign_ex=0; 
-        end
-        if(state==2'b 00)begin
-            PC_source=0;//pc+4
-            MUX_A=1;
-            MUX_B=2'b00;
-            RegWrite=1;
-            MemWrite=1;
-            Reg_MUX=1;
             I_MEM_write=1;
             sign_ex=0;
         end
-    end
+        //______________
 
-    if (temp_I[6:0]==7'b1100011&&temp_I[14:12]==3'b000)begin//
-        if(state==2'b 01)begin
-            PC_source=1;//PC+4
-            MUX_A=0;
-            MUX_B=2'b10;
-            RegWrite=0;
-            MemWrite=0;
-            Reg_MUX=1;
-            ALUOp=4'b0000;
-            I_MEM_write=1;
-            sign_ex=0;
-        end
-        if(state==2'b 10)begin
-            PC_source=0;//PC+4
-            MUX_A=1;
-            MUX_B=2'b10;
-            RegWrite=1;
-            MemWrite=0;
-            Reg_MUX=1;
-            I_MEM_write=0;
-            sign_ex=1;
-        end
-        if(state==2'b 11)begin
-            PC_source=0;//PC+4
-            MUX_A=1;
-            MUX_B=2'b01;
-            RegWrite=0;
-            MemWrite=0;
-            Reg_MUX=1;
-            if(temp_I[14:12]==3'b000)begin//BEQ
-                ALUOp=4'b1100;
-            end
-            if(temp_I[14:12]==3'b001)begin//BNE
-                ALUOp=4'b1011;
-            end
-            if(temp_I[14:12]==3'b100)begin//BLT
-                ALUOp=4'b0111;
-            end
-            if(temp_I[14:12]==3'b101)begin//BGE
-                ALUOp=4'b1000;
-            end
-            if(temp_I[14:12]==3'b110)begin//BLTU
-                ALUOp=4'b1001;
-            end
-            if(temp_I[14:12]==3'b111)begin//BGEU
-                ALUOp=4'b1010;
-            end
-            I_MEM_write=0;
-            sign_ex=1;
-        end
-        if(state==2'b 00)begin
-            PC_source=0;//PC+4
-            MUX_A=1;
-            MUX_B=2'b01;
-            RegWrite=0;
-            MemWrite=1;
-            Reg_MUX=1;
-            I_MEM_write=1;
-            sign_ex=1;
-        end
+
+
+
+
+
+
+
+
+
+        //BEQ................
+        //___________________
     end
-    if (temp_I[6:0]==7'b0100011&&temp_I[14:12]==3'b010)begin//SW
-        if(state==2'b 01)begin
-            PC_source=1;//PC+4
-            MUX_A=0;
-            MUX_B=2'b10;
-            RegWrite=0;
-            MemWrite=0;
-            Reg_MUX=1;
-            ALUOp=4'b0000;
-            I_MEM_write=1;
-            sign_ex=0;
-        end
-        if(state==2'b 10)begin
-            PC_source=0;//PC+4
-            MUX_A=1;
-            MUX_B=2'b10;
-            RegWrite=1;
-            MemWrite=0;
-            Reg_MUX=1;
-            I_MEM_write=0;
-            sign_ex=1;
-        end
-        if(state==2'b 11)begin
-            PC_source=0;//PC+4
-            MUX_A=1;
-            MUX_B=2'b01;
-            RegWrite=0;
-            MemWrite=0;
-            Reg_MUX=1;
-            I_MEM_write=0;
-            sign_ex=1;
-        end
-        if(state==2'b 00)begin
-            PC_source=0;//PC+4
-            MUX_A=1;
-            MUX_B=2'b01;
-            RegWrite=0;
-            MemWrite=1;
-            Reg_MUX=1;
-            I_MEM_write=0;
-            sign_ex=1;
-        end
-    end
-    if (temp_I[6:0]==7'b0000011&&temp_I[14:12]==3'b010)begin//LW
-        if(state==2'b 01)begin
-            PC_source=1;//PC+4
-            MUX_A=0;
-            MUX_B=2'b10;
-            RegWrite=0;
-            MemWrite=0;
-            Reg_MUX=1;
-            ALUOp=4'b0000;
-            I_MEM_write=1;
-            sign_ex=0;
-        end
-        if(state==2'b 10)begin
-            PC_source=0;//PC+4
-            MUX_A=1;
-            MUX_B=2'b10;
-            RegWrite=1;
-            MemWrite=0;
-            Reg_MUX=1;
-            I_MEM_write=0;
-            sign_ex=1;
-        end
-        if(state==2'b 11)begin
-            PC_source=0;//PC+4
-            MUX_A=1;
-            MUX_B=2'b01;
-            RegWrite=0;
-            MemWrite=0;
-            Reg_MUX=1;
-            I_MEM_write=0;
-            sign_ex=1;
-        end
-        if(state==2'b 00)begin
-            PC_source=0;//PC+4
-            MUX_A=1;
-            MUX_B=2'b01;
-            RegWrite=0;
-            MemWrite=1;
-            Reg_MUX=1;
-            I_MEM_write=0;
-            sign_ex=1;
-        end
+    if(state==2'b 00)begin
+        PC_source=0;//pc+4
+        MUX_A=1;
+        MUX_B=2'b00;
+        RegWrite=1;
+        MemWrite=1;
+        Reg_MUX=1;
+        I_MEM_write=1;
+        sign_ex=0;
     end
 end
-
-
-always @(posedge clk) begin
-    state=state+1;
-end
-endmodule //CONTROL
