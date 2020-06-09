fa9c3da50ced0278076c0974913f5a3283d81741
czh9919 <czh9919@outlook.com>
2020-06-09 21:52:59 +0800

    relax

--- lab5/RTL/modules/UNIT.v
+++ lab5/RTL/modules/UNIT.v
@@ -16,17 +16,19 @@ end
 always @(*) begin
     MUXA=0;
     MUXB=0;
+
     if((I3[24:20]==I5[11:7])&&I5[11:7]!=0)begin
         MUXB=2'b10;
+        if (I4[6:0]==7'b0100011&&I4[14:12]==3'b010)begin
+            MUXB=2'b0;
+        end
     end
     if((I3[19:15]==I5[11:7])&&I5[11:7]!=0)begin
         MUXA=2'b10;
-
     end
     if((I3[24:20]==I4[11:7])&&I4[11:7]!=0)begin
         MUXB=2'b01;
-
-	    if (I4[6:0]==7'b0100011&&I4[14:12]==3'b010)begin
+        if (I4[6:0]==7'b0100011&&I4[14:12]==3'b010)begin
             MUXB=2'b0;
         end
     end
